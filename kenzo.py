f8b28f35153abf56f56b092606496217788bb99c
hanzohd <73936655+hanzohd@users.noreply.github.com>
2020-11-05 11:53:59 +0300

    kayzo
    سلام عليكم

---
+++ kenzo.py
@@ -0,0 +1,40 @@
+import os
+print(" _        _______           _______  _______     _________ _______  _______  _       ")
+print("| \    /\(  ___  )|\     /|/ ___   )(  ___  )    \__   __/(  ___  )(  ___  )( \       ")
+print("|  \  / /| (   ) |( \   / )\/   )  || (   ) |       ) (   | (   ) || (   ) || (       ")
+print("|  (_/ / | (___) | \ (_) /     /   )| |   | | _____ | |   | |   | || |   | || |       ")
+print("|   _ (  |  ___  |  \   /     /   / | |   | |(_____)| |   | |   | || |   | || |       ")
+print("|  ( \ \ | (   ) |   ) (     /   /  | |   | |       | |   | |   | || |   | || |       ")
+print("|  /  \ \| )   ( |   | |    /   (_/\| (___) |       | |   | (___) || (___) || (____/\ ")
+print("|_/    \/|/     \|   \_/   (_______/(_______)       )_(   (_______)(_______)(_______/ ")
+print("1 for download mitasploit")
+print("2 for download Xerxes")
+print("3 for download Apk-Binder")
+print("4 for download fb-hacker")
+print("5 for download WhatsappSPAM")
+print("6 for download ip geolocation")
+print("7 for download Malicious")
+print("8 for download DarkPremium")
+print("9 for download Fix-Termux")
+print("10 for download sherlock")
+X = input("type number")
+if X == "1":
+    os.system("git clone https://github.com/rapid7/metasploit-framework")
+if X == "2":
+    os.system("git clone git clone https://github.com/zanyarjamal/xerxes")
+if X == "3":
+    os.system("git clone https://github.com/kinghacker0/Apk-Binder")
+if X == "4":
+    os.system("git clone https://github.com/V4N654T/fb-hacker")
+if X == "5":    
+    os.system("git clone https://github.com/sadamshr3be/WhatsappSPAM")
+if X == "6":
+    os.system("git clone https://github.com/maldevel/IPGeoLoca")
+if X == "7":
+    os.system("git clone https://github.com/MaulanaRyM/Malicious")
+if X == "8"
+    os.system("git clone https://github.com/TheMagizz/DarkPremium")
+if X == "9"
+    os.system("git clone https://github.com/pro-root/Fix-Termux")
+if X == "10"
+     os.system("git clone https://github.com/sherlock-project/sherlock")
